4c39e6bd19f2c89b99991656ead619b84cd4bd54
Julian Sanchez <cjulsan@gmail.com>
2021-06-07 16:44:22 -0500

    commit9

--- src/component/historial.tsx
+++ src/component/historial.tsx
@@ -4,7 +4,9 @@ import React from 'react';
 function Historial(): JSX.Element {
 
     return (
-        <div></div>
-      );
-}
+        <div>
+
+        </div>
+      )
+};
 export default Historial;
