47abbc460c33dbd23fde83a56206afa816bda2cf
Julian Sanchez <cjulsan@gmail.com>
2021-06-07 16:38:50 -0500

    commit8

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,22 @@ import React from 'react';
 import './App.css' 
 import Tiempo  from './component/tiempo';
 import Historial from './component/historial';
-import  {Button} from '@material-ui/core';
+import  {Button, Menu} from '@material-ui/core';
 
 
 
 function App():JSX.Element{
   return(
-   <div></div> 
+   <div>
+    <div> 
+        <Tiempo/>
+        </div>
+       <div>
+       <Button variant="contained" color="primary">
+        Hola Mundo!
+      </Button>
+      </div> 
+   </div> 
   )
 };
 
