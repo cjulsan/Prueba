da2694f1c324d30772d07b721df0c278cde3634b
Julian Sanchez <cjulsan@gmail.com>
2021-06-07 15:44:52 -0500

    commit3

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,16 @@ import React from 'react';
 import './App.css' 
 import Tiempo  from './component/tiempo';
 import Historial from './component/historial';
+import  {Button} from '@material-ui/core';
 
 
 
-function App(){
-  return (
-   <div>
-    <Tiempo/>
-    <Historial />
-   </div>
- )
+function App():JSX.Element{
+  return(
+    <Button variant="contained" color="primary">
+    Hola Mundo!
+  </Button>
+  )
 };
 
 export default App;
--- src/component/historial.tsx
+++ src/component/historial.tsx
@@ -1,24 +1,10 @@
 import { JsxElement } from "typescript";
 import React from 'react';
-import form, { Button } from '@material-ui/core';
-
-
-
 
 function Historial(): JSX.Element {
 
     return (
-        
-            <form>
-            <p>Nombre: <input type="text" name="nombre" size="40"></p>
-
-
-            <Button variant="contained" color="primary">
-                     Consultar
-           </Button>
-           </form>
-          
+        <div></div>
       );
 }
-
 export default Historial;
