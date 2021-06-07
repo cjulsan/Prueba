d8ea01163fc36ab483055275a27e25b80add8d95
Julian Sanchez <cjulsan@gmail.com>
2021-06-07 16:46:35 -0500

    commit10

--- src/component/historial.tsx
+++ src/component/historial.tsx
@@ -1,11 +1,11 @@
 import { JsxElement } from "typescript";
 import React from 'react';
 
-function Historial(): JSX.Element {
+function Historial():JSX.Element {
 
     return (
         <div>
-
+         historial
         </div>
       )
 };
