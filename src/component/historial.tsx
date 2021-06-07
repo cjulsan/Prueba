bb98212e0e0a74326e65307c76fc3ba0f87b9574
Julian Sanchez <cjulsan@gmail.com>
2021-06-07 14:32:42 -0500

    formulario agregado

---
+++ src/component/historial.tsx
@@ -0,0 +1,11 @@
+import { JsxElement } from "typescript";
+
+function Historial():JSX.Element{
+    return(
+        <div> 
+        <h3>HISTORIAL</h3>
+        </div>
+    )
+};
+
+export default Historial; 
---
+++ src/component/tiempo.tsx
@@ -0,0 +1,13 @@
+//import { JsxElement } from "typescript";
+
+const initialDb = [];
+
+    function Tiempo() {
+    return(
+        <div> 
+            <h3>ESTADO DEL TIEMPO Y NOTICIAS</h3>
+        </div>
+    )
+};
+
+export default Tiempo; 
