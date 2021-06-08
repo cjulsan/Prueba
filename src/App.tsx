810b285f0c64915d3731427d2ce8ec9d43acd591
Julian Sanchez <cjulsan@gmail.com>
2021-06-08 05:01:07 -0500

    commit18

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
 
 import React from 'react';
-import Menu from './component/menu';
 import './App.css'
+import Noticias from './component/noticias';
+import Tiempo from './component/tiempo';
+import Historial from './component/historial';
+import { Button, ButtonGroup, TextField, BottomNavigation, BottomNavigationAction } from '@material-ui/core';
 
 
 function App(): JSX.Element {
   return (
     <div className="App">
-      <Menu/>
-    </div>
+            <div>
+                Aplicacion
+            </div>
+            <div>
+                <form>
+                    <TextField id="Ciudad" label="Ciudad" variant="filled" color="primary"/>
+                </form>
+
+            </div>
+            <div className="centrado">
+                <ButtonGroup disableElevation variant="contained" color="primary">
+                    <Button onClick={() => "Noticias.html"}>Consultar Noticias</Button>
+                    <Button onClick={() => "Tiempo.html"}>Consultar Tiempo</Button>
+                    <Button onClick={() => "Historial.html"}>Historial</Button>
+                </ButtonGroup>
+            </div>
+        </div>
   )
 };
 export default App;
