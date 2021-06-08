faf90ea51793a2bdb58e59bad958158750606cd1
Julian Sanchez <cjulsan@gmail.com>
2021-06-08 04:03:56 -0500

    commit17

--- src/component/menu.tsx
+++ src/component/menu.tsx
@@ -3,7 +3,6 @@ import Tiempo from './tiempo';
 import Historial from './historial';
 import Noticias from './noticias';
 import { Button, ButtonGroup, TextField, BottomNavigation, BottomNavigationAction } from '@material-ui/core';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function Menu() {
 
@@ -11,7 +10,7 @@ function Menu() {
         <div className="App">
             <div>
                 Aplicacion
-    </div>
+            </div>
             <div>
                 <form>
                     <TextField id="Ciudad" label="Ciudad" variant="filled" color="primary" background-Color="gray" />
@@ -24,25 +23,7 @@ function Menu() {
                     <Button onClick={() => Tiempo}>Consultar Tiempo</Button>
                     <Button onClick={() => Historial}>Historial</Button>
                 </ButtonGroup>
-
             </div>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/about">About</Link>
-                    </li>
-                    <li>
-                        <Link to="/users">Users</Link>
-                    </li>
-                    <li>
-                        <Link to="/hola-mundo">Hello</Link>
-                    </li>
-                </ul>
-            </nav>
-
         </div>
 
     )
