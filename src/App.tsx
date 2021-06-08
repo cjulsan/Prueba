ebbf6a49e78e27c6cc9e19f9f808d54ed248cec7
Julian Sanchez <cjulsan@gmail.com>
2021-06-07 20:31:21 -0500

    commit13

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,40 @@
 
 import React from 'react';
-import './App.css' 
-import Tiempo  from './component/tiempo';
+import './App.css'
+import Tiempo from './component/tiempo';
 import Historial from './component/historial';
 import Noticias from './component/noticias';
-import  {Button, ButtonGroup,TextField} from '@material-ui/core';
+import { Button, ButtonGroup, TextField, BottomNavigation, BottomNavigationAction } from '@material-ui/core';
+import {classes, value, setValue} from '@material-ui/core'
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      <div>
+        <Tiempo />
+        <Historial />
+      </div>
+      <div>
+        <form>
+          <TextField id="Ciudad" label="Ciudad" variant="filled" color="primary" background-Color="gray" />
+        </form>
 
-
-
-function App():JSX.Element{
-  return(
-   <div className ="App">
-    <div> 
-        <Tiempo/>
-        <Historial/>
-        </div>
-        <div>
-          <form>
-            <TextField id="Ciudad" label="Ciudad" variant="filled" color="primary" background-Color="gray"/>
-          </form>  
-               
-        </div>
-       <div className="centrado">
-            <ButtonGroup disableElevation variant="contained" color="primary">
-            <Button onClick={() => Noticias}>Consultar Noticias</Button>
-            <Button onClick={() => Tiempo}>Consultar Tiempo</Button>
-            <Button onClick={() => Historial}>Historial</Button>
-            </ButtonGroup>
-        </div> 
-   </div> 
+      </div>
+      <div className="centrado">
+        <BottomNavigation
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+          showLabels
+          className={classes.root}
+        >
+          <BottomNavigationAction label="Noticias" icon={<Noticias />} />
+          <BottomNavigationAction label="Tiempo" icon={<Tiempo />} />
+          <BottomNavigationAction label="Historial" icon={<Historial />} />
+        </BottomNavigation>
+   
+      </div>
+    </div>
   )
 };
 
