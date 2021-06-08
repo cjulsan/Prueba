92fbc47f4e190b9cf956bce661388c3b9647a36c
Julian Sanchez <cjulsan@gmail.com>
2021-06-07 19:22:27 -0500

    commit11

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,31 @@ import React from 'react';
 import './App.css' 
 import Tiempo  from './component/tiempo';
 import Historial from './component/historial';
-import  {Button, Menu} from '@material-ui/core';
+import Noticias from './component/noticias';
+import  {Button, ButtonGroup,TextField} from '@material-ui/core';
 
 
 
 function App():JSX.Element{
   return(
-   <div>
+   <div className ="App">
     <div> 
         <Tiempo/>
+        <Historial/>
         </div>
-       <div>
-       <Button variant="contained" color="primary">
-        Hola Mundo!
-      </Button>
-      </div> 
+        <div>
+          <form>
+            <TextField id="Ciudad" label="Ciudad" variant="filled" color="primary" background-Color="gray"/>
+          </form>  
+               
+        </div>
+       <div className="centrado">
+            <ButtonGroup disableElevation variant="contained" color="primary">
+            <Button onClick={() => Noticias}>Consultar Noticias</Button>
+            <Button onClick={() => Tiempo}>Consultar Tiempo</Button>
+            <Button onClick={() => Historial}>Historial</Button>
+            </ButtonGroup>
+        </div> 
    </div> 
   )
 };
---
+++ src/component/noticias.tsx
@@ -0,0 +1,10 @@
+import { JsxElement } from "typescript";
+
+function Noticias():JSX.Element{
+    return(
+        <div> 
+        <h3>En construccion</h3>
+        </div>
+    )
+};
+export default Noticias;
