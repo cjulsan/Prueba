5cc9de2c4c34333fa94db4be1eb6a7bdb5b24a23
Julian Sanchez <cjulsan@gmail.com>
2021-06-07 21:08:56 -0500

    commit15

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,9 @@ import './App.css'
 import Tiempo from './component/tiempo';
 import Historial from './component/historial';
 import Noticias from './component/noticias';
+import React, {useState} from 'react';
 import { Button, ButtonGroup, TextField, BottomNavigation, BottomNavigationAction } from '@material-ui/core';
-import {classes, value, setValue} from '@material-ui/core'
+
 function App(): JSX.Element {
   return (
     <div className="App">
@@ -20,22 +21,29 @@ function App(): JSX.Element {
 
       </div>
       <div className="centrado">
-        <BottomNavigation
-          value={value}
-          onChange={(event, newValue) => {
-            setValue(newValue);
-          }}
-          showLabels
-          className={classes.root}
-        >
-          <BottomNavigationAction label="Noticias" icon={<Noticias />} />
-          <BottomNavigationAction label="Tiempo" icon={<Tiempo />} />
-          <BottomNavigationAction label="Historial" icon={<Historial />} />
-        </BottomNavigation>
-   
+        <ButtonGroup disableElevation variant="contained" color="primary">
+          <Button onClick={() => Noticias}>Consultar Noticias</Button>
+          <Button onClick={() => Tiempo}>Consultar Tiempo</Button>
+          <Button onClick={() => Historial}>Historial</Button>
+          </ButtonGroup>
+
       </div>
     </div>
   )
 };
+
+const Parent:React.FC = () => {
+  const [parentName, setParentName] = useState<string>('Mr John Obi');
+  const updateName = (name: string):void => {
+       setParentName(name)
+  }
+  return (
+       <div>
+         <FirstChild name={parentName} updateName={updateName} />
+         <SecondChild name={parentName} updateName={updateName} />
+         <ThirdChild name={parentName} updateName={updateName} />
+      </div>
+  )
+  }
 
 export default App;
