4df8d71b6f5cc714025c5806e1a10418d8268bdf
Julian Sanchez <cjulsan@gmail.com>
2021-06-10 14:53:37 -0500

    commit45

--- src/component/tiempo.tsx
+++ src/component/tiempo.tsx
@@ -1,7 +1,6 @@
 
 import React, {useState, useEffect} from 'react';
 
-
 function Tiempo(){
     const url = 'http://api.openweathermap.org/data/2.5/weather?id=524901&appid=0cab44a0fbf8250e98acea9b1b6be75d&lang={es}'
   const [todos, setTodos] = useState<any>() 
