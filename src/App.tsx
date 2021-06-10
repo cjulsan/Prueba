2a5779d31fa6b7a98cf23e88e4a2f455af64c138
Julian Sanchez <cjulsan@gmail.com>
2021-06-10 01:04:43 -0500

    commit33

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,29 @@ import './App.css'
 import Historial from './component/historial';
 import Noticias from './component/noticias';
 import Tiempo from './component/tiempo';
-import { BrowserRouter, Route, Link} from "react-router-dom";
-import { Button, ButtonGroup } from '@material-ui/core';
-import { Fragment } from 'react';
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+
+function App() {
 
 
-function App(){
-   
+    <BrowserRouter>
+      <Switch>>
+        <Route path="/tiempo" component={Tiempo} />
+        <Route path="/noticias" component={Noticias} />
+        <Route path="/historial" component={Historial} />
+      </Switch>
+
+    </BrowserRouter>
+
   return (
     <div>
-      <BrowserRouter>
-        <div> <Fragment>
-          <Route path="/tiempo" component={Tiempo} />
-          <Route path="/noticias" component={Noticias} />
-          <Route path="/historial" component={Historial} />
-        </Fragment>
-        </div>
-        <div><Fragment>
-          <li><Link to ="/noticias"> Consultar Noticias</Link></li>
-          <li><Link to ="/tiempo"> Consultar Tiempo</Link></li>
-          <li><Link to ="/historial"> Historial</Link></li>
-        </Fragment>
-       </div>
-      </BrowserRouter>
+      <li><Link to="/noticias"> Consultar Noticias</Link></li>
+      <li><Link to="/tiempo"> Consultar Tiempo</Link></li>
+      <li><Link to="/historial"> Historial</Link></li>
     </div>
-  );
-}
+  )
+};
+
 export default App();
+
+
