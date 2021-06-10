ba038975665c2195f5f12e6bfaac5dafad5100df
Julian Sanchez <cjulsan@gmail.com>
2021-06-10 07:52:01 -0500

    commit33

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,28 @@ import Historial from './component/historial';
 import Noticias from './component/noticias';
 import Tiempo from './component/tiempo';
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
-type FormElement = React.FormEvent<HTMLFormElement>;
+import  React from 'react';
 
-function App(){
-{
-    <BrowserRouter>
-      <Switch>
-        <Route path="/tiempo" component={Tiempo} />
-        <Route path="/noticias" component={Noticias} />
-        <Route path="/historial" component={Historial} />
-      </Switch>
-    </BrowserRouter>
-}
+function App() {
+
   return (
     <div>
+       <React.Fragment>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/tiempo" component={Tiempo} />
+          <Route path="/noticias" component={Noticias} />
+          <Route path="/historial" component={Historial} />
+        </Switch>
+      </BrowserRouter>     
+      <ul>
       <li><Link to="/noticias"> Consultar Noticias</Link></li>
       <li><Link to="/tiempo"> Consultar Tiempo</Link></li>
       <li><Link to="/historial"> Historial</Link></li>
+      </ul>
+      </React.Fragment> 
     </div>
-  )
+  );
 };
 export default App;
 
